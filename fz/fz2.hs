8ec5e1ea52d7f56e0d7a588d1d3dde76d6550021
Ayman Osman <aymano.osman@gmail.com>
2016-02-17 23:08:57 +0000

    wip

--- fz/fz2.hs
+++ fz/fz2.hs
@@ -1,34 +1,9 @@
 import Data.Maybe
-import Data.Monoid
-import Control.Monad
 
+main =  mapM_ (putStrLn . fz) [1..100]
 
-main = run 15
+fz n = fromMaybe (show n) (fizzer n)
 
-run a =
- forM_ lines putStrLn
- where
-   nums = [1..a]
-   xs = zip nums (map fz nums)
-   lines =
-     map (\(a, b) -> show a <> " " <> b)
-     xs
+fizzer = mconcat [ 3 ~> "Fizz" , 5 ~> "Buzz" , 7 ~> "Hiss"]
 
-
-fz n =
-  fromMaybe (show n) (fizzer n)
-
-
-fizzer =
-  mconcat
-  [ (3 ~> "Fizz")
-  , (5 ~> "Buzz")
-  , (7 ~> "Hiss")
-  ]
-
-
-(~>) :: Int -> String -> Int -> Maybe String
-(~>) m s n =
-  if mod n m == 0
-  then Just s
-  else Nothing
+(~>) m s n = if mod n m == 0 then Just s else Nothing
