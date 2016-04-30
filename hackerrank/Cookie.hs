1c0e2e0fa3ccc10faed24873a8df1df8148d104b
Ayman Osman <aymano.osman@gmail.com>
2016-04-30 11:31:00 +0100

    naive solution and quickcheck props

--- hackerrank/Cookie.hs
+++ hackerrank/Cookie.hs
@@ -1,7 +1,9 @@
 module Cookie where
 
 import qualified Data.List as List
+import Debug.Trace (trace)
 import qualified Data.Heap as H
+import Test.QuickCheck (quickCheck)
 
 main' :: IO ()
 main' =
@@ -45,6 +47,21 @@ step m heap
       then Done heap
       else More $ H.insert (a+2*b) h'
 
+naive :: Int -> Int -> [Int] -> Int
+naive n m l
+  | not (null l) && all (>m) l = -- expect sorted list
+    n
+  | otherwise =
+    case l of
+      a:b:xs ->
+        let l' = List.sort ((a+2*b):xs)
+        in
+        naive (n+1) m
+        -- $ trace ("l: " ++ show l') l'
+        $ l'
+      _ ->
+        -1
+
 type Heap = H.MinHeap Int
 data Ans =
   Done Heap
@@ -57,12 +74,32 @@ main =
        l = [12, 10, 9, 3, 2, 1]
        h = H.fromList l
        x = ans' 0 7 (H.fromList l)
+       y = naive 0 7 (List.sort l)
        -- y = ansNaive 0 7 (List.sort l)
      print x
+     print y
      putStrLn "==="
      print $ ans' 0 10 h
      print $ ans' 0 20 h
      print $ ans' 0 100 h
      print $ ans' 0 110 h
-     -- print y
+     putStrLn "=== QuickCheck ==="
+     quickCheck prop_same_as_naive
 
+
+-- tests
+{-
+ failed: [-5, 4, 2]
+ -}
+prop_same_as_naive :: [Int] -> Bool
+prop_same_as_naive l =
+  let
+    a = ans 7 l
+    b = naive 0 7 (List.sort l)
+
+  in
+  -- trace ("ans: " ++ show a) a
+  a
+  ==
+  b
+  -- trace ("naive: " ++ show b) b
