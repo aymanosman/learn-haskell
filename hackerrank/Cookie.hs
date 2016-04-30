22f0869020e64139e3f20f1925cc052a22621e24
Ayman Osman <aymano.osman@gmail.com>
2016-04-30 10:21:30 +0100

    wip

--- hackerrank/Cookie.hs
+++ hackerrank/Cookie.hs
@@ -1,14 +1,17 @@
 module Cookie where
 
+import qualified Data.List as List
+
 main' :: IO ()
 main' =
-  do let readInts = (fmap read) `fmap` words `fmap` getLine :: IO [Int]
+  do let readInts = fmap read `fmap` words `fmap` getLine :: IO [Int]
      [_, sweetness] <- readInts
      cs <- readInts
      -- make heap?
      -- assert length cs == _ above
-     putStrLn $ toString (ans sweetness cs)
+     putStrLn $ toString (ans sweetness (List.sort cs))
 
+toString :: Show a => Maybe (a, t) -> String
 toString Nothing = "-1"
 toString (Just (n, _)) = show n
 
@@ -16,8 +19,8 @@ toString (Just (n, _)) = show n
 ans :: Int -> [Int] -> Maybe (Int, [Int])
 ans =
   ans' 0
-  where
 
+ans' :: (Num a, Num t, Ord a) => t -> a -> [a] -> Maybe (t, [a])
 ans' n sweetness l
   | all (>=sweetness) l = Just (n, l)
   | otherwise =
@@ -28,6 +31,7 @@ ans' n sweetness l
          Nothing
 
 --
+main :: IO ()
 main =
   do let x = ans 7 [1, 2, 3, 9, 10, 12]
      print x
