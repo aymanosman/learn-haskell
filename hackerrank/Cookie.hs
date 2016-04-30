15aac5988774b123d6d92ae65b1806257d382a98
Ayman Osman <aymano.osman@gmail.com>
2016-04-30 13:16:39 +0100

    min heap implementation

--- hackerrank/Cookie.hs
+++ hackerrank/Cookie.hs
@@ -2,18 +2,16 @@ module Main where
 
 import qualified Data.List as List
 import Debug.Trace (trace)
-import qualified Data.Heap as H
+-- import qualified Data.Heap as H
 import Test.QuickCheck  -- (quickCheck)
-
-type Heap = H.MinHeap Int
 
 data Ans =
   Done Heap
   | More Heap
   | Fail
 
-main :: IO ()
-main =
+main' :: IO ()
+main' =
   do let readInts = fmap read `fmap` words `fmap` getLine :: IO [Int]
      [_, sweetness] <- readInts
      cs <- readInts
@@ -22,7 +20,7 @@ main =
 
 ans :: Int -> [Int] -> Int
 ans m l =
-  case ans' 0 m (H.fromList l) of
+  case ans' 0 m (fromList l) of
     Nothing -> -1
     Just (n, _) -> n
 
@@ -37,22 +35,22 @@ ans' n sweetness h =
     More heap ->
       ans' (n+1) sweetness heap
 
-step :: Int -> H.MinHeap Int -> Ans
+step :: Int -> Heap -> Ans
 step m heap
-  | H.size heap < 2 =
-    case H.viewHead heap of
+  | size heap < 2 =
+    case viewHead heap of
       Nothing -> Fail
       Just n ->
         if n >= m
         then Done heap
         else Fail
   | otherwise =
-    let Just (a, h) = H.view heap
-        Just (b, h') = H.view h
+    let Just (a, h) = view heap
+        Just (b, h') = view h
     in
       if a >= m && b >= m
       then Done heap
-      else More $ H.insert (a+2*b) h'
+      else More $ insert (a+2*b) h'
 
 -- tests
 
@@ -71,8 +69,8 @@ naive n m l
 
       _ -> -1
 
-main' :: IO ()
-main' =
+main :: IO ()
+main =
   do putStrLn "==="
      print $ ans (10^7) [1,2,3,9,10,12]
      print $ ans 0 [1,2]
@@ -92,3 +90,62 @@ prop_same_as_naive m' l' =
   ==
   -- trace ("naive: " ++ show b)
   b
+
+-- min heap implementation
+
+data Heap =
+  Empty
+  | Node !Int !Int Heap Heap -- size then val, change to record for clarity
+  deriving (Show)
+
+empty :: Heap
+empty = Empty
+
+singleton :: Int -> Heap
+singleton n =
+  Node 1 n empty empty
+{-# INLINE singleton #-}
+
+size :: Heap -> Int
+size (Node s _ _ _) = s
+size _ = 0
+
+insert :: Int -> Heap -> Heap
+insert n =
+  merge (singleton n)
+
+view :: Heap -> Maybe (Int, Heap)
+view (Node _size n l r) = Just (n, merge l r)
+view _ = Nothing
+
+viewHead :: Heap -> Maybe Int
+viewHead (Node _ n _ _) = Just n
+viewHead _ = Nothing
+
+deleteMin :: Heap -> Heap
+deleteMin (Node _ _ l r) =
+  merge l r
+deleteMin _ =
+  error "empty"
+
+merge :: Heap -> Heap -> Heap
+merge Empty h = h
+merge h Empty = h
+merge h@(Node s1 n l r) g@(Node s2 m l' r')
+  | n < m     = Node (s1+s2) n (merge g l) r
+  | otherwise = Node (s1+s2) m (merge h l') r'
+
+-- Conversions
+fromList :: [Int] -> Heap
+fromList =
+  foldr (merge . singleton) Empty
+
+toList :: Heap -> [Int]
+toList Empty = []
+toList (Node _ a l r) =
+  a : toList (merge l r)
+
+-- Tests
+prop_sorted :: [Int] -> Bool
+prop_sorted l =
+  (toList . fromList) l == List.sort l
