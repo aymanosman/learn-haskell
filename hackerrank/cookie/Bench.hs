acc53e9c9f95bfddcc1d8530204071e91db7e1c5
Ayman Osman <aymano.osman@gmail.com>
2016-05-02 09:44:00 +0100

    wip

--- hackerrank/cookie/Bench.hs
+++ hackerrank/cookie/Bench.hs
@@ -5,6 +5,7 @@ import qualified System.Random.Mersenne as M
 import System.Environment
 import qualified Data.Judy as J
 import Control.Monad
+import Control.DeepSeq
 import qualified Data.Heap as H
 
 import Data.Binary
@@ -13,10 +14,12 @@ import qualified Data.ByteString.Lazy as BS
 import qualified Data.PQueue.Max as PQ
 
 -- TODO: compare to heap and stuff
-n = 1000000
+binaryFile = "binary.txt"
+textFile = "text.txt"
+n = 100--0000
 
 pqueue = do
-  s <- BS.readFile "sample.txt"
+  s <- BS.readFile binaryFile
   let rs = decode s :: [Word]
   let pq = PQ.fromList rs
   let Just (v, _) = PQ.maxView pq
@@ -29,39 +32,47 @@ main = do
     "judy-direct" -> judyDirect
     "heap" -> heap
     "pqueue" -> pqueue
-    "sample" -> writeNums
+    "binary" -> writeBinary
+    "text" -> writeText
     _ -> error "failed"
 
 judy = do
-   s <- BS.readFile "sample.txt"
-   let rs = decode s :: [Word]
-   j  <- J.new :: IO (J.JudyL Int)
-   forM_ rs $ \n ->
-       J.insert n 1 j
-   Just (v, _)  <- J.findMax j
-   print v
+  s <- BS.readFile binaryFile
+  let rs = decode s :: [Word]
+  j  <- J.new :: IO (J.JudyL Int)
+  forM_ rs $ \n ->
+      J.insert n 1 j
+  Just (v, _)  <- J.findMax j
+  print v
 
 judyDirect = do
-   g <- M.getStdGen
-   rs <- M.randoms g
-   j  <- J.new :: IO (J.JudyL Int)
-   forM_ (take n rs) $ \n ->
-       J.insert n 1 j
-   Just (v, _)  <- J.findMax j
-   print v
+  g <- M.getStdGen
+  rs <- M.randoms g
+  j  <- J.new :: IO (J.JudyL Int)
+  forM_ (take n rs) $ \n ->
+      J.insert n 1 j
+  Just (v, _)  <- J.findMax j
+  print v
 
 
 heap = do
-   s <- BS.readFile "sample.txt"
-   let rs = decode s :: [Word]
-   let h = H.fromList rs :: H.MaxHeap Word
-   let Just w = H.viewHead h
-   print w
+  s <- BS.readFile binaryFile
+  let rs = decode s :: [Word]
+  let h = H.fromList rs :: H.MaxHeap Word
+  let Just w = H.viewHead h
+  print w
 
-writeNums = do
-   g  <- getStdGen
-   let rs = randoms g :: [Word]
-   BS.writeFile "sample.txt" $ encode (take n rs)
+writeBinary = do
+  g  <- getStdGen
+  let rs = randoms g :: [Word]
+  BS.writeFile binaryFile $ encode (take n rs)
+
+writeText = do
+  g  <- getStdGen
+  let rs = randoms g :: [Word]
+  let s = "1000000 42424242\n" ++ (unwords $ map show (take n rs))
+  writeFile textFile s
+
 
 
 
