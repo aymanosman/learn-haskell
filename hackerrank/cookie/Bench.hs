ab00c14621535a15de4f0d4ddaf2a573d8d1eea6
Ayman Osman <aymano.osman@gmail.com>
2016-05-02 09:13:55 +0100

    bench pqueue is REALLY fast

--- hackerrank/cookie/Bench.hs
+++ hackerrank/cookie/Bench.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Random
+import qualified System.Random.Mersenne as M
 import System.Environment
 import qualified Data.Judy as J
 import Control.Monad
@@ -9,35 +10,57 @@ import qualified Data.Heap as H
 import Data.Binary
 import qualified Data.ByteString.Lazy as BS
 
+import qualified Data.PQueue.Max as PQ
+
 -- TODO: compare to heap and stuff
 n = 1000000
+
+pqueue = do
+  s <- BS.readFile "sample.txt"
+  let rs = decode s :: [Word]
+  let pq = PQ.fromList rs
+  let Just (v, _) = PQ.maxView pq
+  print v
 
 main = do
   [which] <- getArgs
   case which of
     "judy" -> judy
+    "judy-direct" -> judyDirect
     "heap" -> heap
+    "pqueue" -> pqueue
+    "sample" -> writeNums
     _ -> error "failed"
 
 judy = do
-   g  <- getStdGen
-   let rs = randoms g
+   s <- BS.readFile "sample.txt"
+   let rs = decode s :: [Word]
+   j  <- J.new :: IO (J.JudyL Int)
+   forM_ rs $ \n ->
+       J.insert n 1 j
+   Just (v, _)  <- J.findMax j
+   print v
+
+judyDirect = do
+   g <- M.getStdGen
+   rs <- M.randoms g
    j  <- J.new :: IO (J.JudyL Int)
    forM_ (take n rs) $ \n ->
        J.insert n 1 j
    Just (v, _)  <- J.findMax j
    print v
 
+
 heap = do
-   g  <- getStdGen
-   let rs = randoms g
-   let h = H.fromList (take n rs) :: H.MaxHeap Int
+   s <- BS.readFile "sample.txt"
+   let rs = decode s :: [Word]
+   let h = H.fromList rs :: H.MaxHeap Word
    let Just w = H.viewHead h
    print w
 
-write_nums = do
+writeNums = do
    g  <- getStdGen
-   let rs = randoms g :: [Int]
+   let rs = randoms g :: [Word]
    BS.writeFile "sample.txt" $ encode (take n rs)
 
 
