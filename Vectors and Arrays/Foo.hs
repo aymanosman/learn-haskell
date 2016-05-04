be2f7a98ea537968e84c8c8b13b031dda05d1329
Ayman Osman <aymano.osman@gmail.com>
2016-05-04 08:36:17 +0100

    Data.Array.ST

--- Vectors and Arrays/Foo.hs
+++ Vectors and Arrays/Foo.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.Environment (getArgs)
 import System.Random
 import Control.Monad
 
@@ -8,17 +9,26 @@ import Data.Array.ST
 import Data.Array.Unboxed
 import Data.Array.MArray.Heapsort (sort)
 
-size = 1000000
+-- size = 1000000
 
 main :: IO ()
-main = do
-  xs <- replicateM size $ randomRIO (1,100) :: IO [Int]
-  -- print (List.sort xs)
-  let arr2 = gg xs
-  print (elems arr2)
+main =
+  run 1000
+  -- program
 
-gg :: [Int] -> UArray Int Int
-gg xs = runSTUArray $ do
+program = do
+  [n'] <- getArgs
+  let size = read n' :: Int
+  run size
+
+run size = do
+  xs <- replicateM size $ randomRIO (1,100) :: IO [Int]
+  -- print $ length $ (List.sort xs)
+  let arr2 = gg size xs
+  print $ arr2 ! (size`div`2)
+
+gg :: Int -> [Int] -> UArray Int Int
+gg size xs = runSTUArray $ do
   arr <- newListArray (1,size) xs
   sort arr
   return arr
