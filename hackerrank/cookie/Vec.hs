601572069507f11e74e88f18fef079058d806abd
Ayman Osman <aymano.osman@gmail.com>
2016-05-04 08:35:57 +0100

    Vec.hs

--- hackerrank/cookie/Vec.hs
+++ hackerrank/cookie/Vec.hs
@@ -1,19 +1,17 @@
--- import Control.Monad
-import Control.Monad.ST
-import Data.Attoparsec.Text as P
 -- import Control.Applicative
-import Data.Text.Encoding
-import System.IO
+import System.IO (openFile, IOMode(ReadMode), Handle)
 import qualified Data.ByteString as B
+import Data.Text.Encoding (decodeUtf8)
+import qualified Data.Attoparsec.Text as P
 
-import Data.Vector.Unboxed as V
-import Data.Vector.Unboxed.Mutable as MV
-import Data.Vector.Algorithms.Heap as VH
-import qualified Data.Vector.Generic         as G
-import qualified Data.Vector.Generic.Mutable as GM
+import Control.Monad
+import Control.Monad.ST
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector.Algorithms.Heap as VH
 
 main =
-  run
+  run1
 
 run = openFile "text.txt" ReadMode >>= run'
 
@@ -23,15 +21,22 @@ run' h = do
   let v = V.fromList cs
   print $ V.take 10 $ v
 
+run1 = do
+  let size = 6
+  v <- MV.new size
+  forM_ (Prelude.zip [0..size-1] [1,9,10,2,12,3]) $ \(i, e) ->
+    MV.unsafeWrite v i e
+  VH.sort v
+  forM_ [0..size-1] $ \i -> print =<< (MV.unsafeRead v i)
+  putStrLn "==="
+  -- pop compare v 0 size-1
+  -- heapify compare v 0 size-1
+
+  forM_ [0..size-1] $ \i -> print =<< (MV.unsafeRead v i)
+  return v
+
 readWords :: Handle -> IO (Either String [Word])
 readWords h =
   P.parseOnly (P.many1 (P.decimal <* P.skipSpace)) . decodeUtf8
   <$> B.hGetLine h
-
--- heapsort = undefined
-
--- main =
---   -- print $ runST ff
---   do h <- openFile "text.txt" ReadMode
---      run h
 
