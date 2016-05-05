f503fbda8f73ac00ed1d26305ce16e5cb1ac05ba
Ayman Osman <aymano.osman@gmail.com>
2016-05-05 09:49:57 +0100

    heapsort

---
+++ Vectors and Arrays/HeapSort.hs
@@ -0,0 +1,145 @@
+module Main where
+-- module Heapsort (heapsort, heapsort') where
+
+
+import System.Random (randomRIO)
+import System.Environment (getArgs)
+import Control.Monad (replicateM, when)
+import qualified Data.List as List
+import Data.Array.MArray
+import qualified Data.Array.IO as A
+
+import Criterion.Main (nfIO)
+import Criterion.Measurement (measure, secs)
+import Criterion.Types (Measured(..))
+import Control.DeepSeq (NFData)
+
+time :: String -> IO () -> IO ()
+time s act =
+  do (m, _) <- measure (nfIO act) 1
+     putStr (s++": ")
+     putStrLn $ secs $ measTime m
+
+maxNum = 1000000 -- 1 million
+
+main = do
+  [which, n'] <- getArgs
+  let n = read n'
+  case which of
+    "array" ->
+      time "array" $ runArray n
+    "list" ->
+      time "list" $ runList n
+
+runArray size = do
+  xs <- replicateM size (randomRIO (1,maxNum)) :: IO [Int]
+  arr <- A.newListArray (1,size) xs :: IO (A.IOArray Int Int)
+  -- swap (1,size) arr
+  heapsort arr
+  -- print =<< A.getElems arr
+  print =<< A.readArray arr 1
+  -- print =<< A.readArray arr size
+
+runList size = do
+  xs <- replicateM size (randomRIO (1,maxNum)) :: IO [Int]
+  let l = List.sort xs
+  print $ head l
+
+swap :: (Int, Int) -> A.IOArray Int Int -> IO ()
+swap (n,m) arr = do
+  x <- A.readArray arr n
+  y <- A.readArray arr m
+  A.writeArray arr m x
+  A.writeArray arr n y
+
+
+
+data NodeType = Inner | Leaf | Edge
+
+{-# INLINE heapsort #-}
+heapsort :: (Integral i, Ix i, Ord e, MArray a e m) => a i e -> m ()
+-- | Sort the elements of the given 'MArray' in increasing order.
+heapsort a = getBounds a >>= uncurry (heapsort' a)
+
+{-# INLINE heapsort' #-}
+heapsort' :: (Integral i, Ix i, Ord e, MArray a e m) => a i e -> i -> i -> m ()
+-- | Sort the elements of the given 'MArray' in increasing order.
+heapsort' a mn mx = do
+    heapify
+    extract
+  where
+    getN = mx - mn + 1
+
+    {-# INLINE right #-}
+    right i = 2 * i + 2
+
+    {-# INLINE nodeType #-}
+    nodeType n i = case compare (right i) n of
+        GT  -> Leaf
+        EQ  -> Edge
+        LT  -> Inner
+
+    {-# INLINE atIndex #-}
+    atIndex i = readArray a (i + mn)
+
+    {-# INLINE atIndexW #-}
+    atIndexW i = writeArray a (i + mn)
+
+    {-# INLINE downHeap #-}
+    downHeap n j = do
+        c <- atIndex j
+        dh c j
+      where
+        -- Propagate @c@ down the heap. We only move elements up as we walk on
+        -- the nodes, and when we're finished, we write @c@ to the last node.
+        -- This way we save half of read/writes, compared to swapping elements.
+        dh c = dhStep
+          where
+            dhStep i = case nodeType n i of
+                Leaf -> stop
+                Edge -> do
+                    l <- atIndex il
+                    if c < l
+                      then atIndexW i l >> atIndexW il c
+                      else stop
+                Inner -> do
+                    l <- atIndex il
+                    r <- atIndex ir
+                    if c >= l && c >= r
+                      then stop
+                      else if l >= r
+                        then atIndexW i l >> dhStep il
+                        else atIndexW i r >> dhStep ir
+              where
+                {-# INLINE stop #-}
+                stop = when (i /= j) (atIndexW i c)
+                ir = right i
+                il = ir - 1
+
+    {-# INLINE heapify #-}
+    heapify = heapifyRoots (n - 1) -- optimize: use 2^k such that k is maximal that 2^k < n
+      where
+        n = getN
+        heapifyRoots i | i < 0  = return ()
+                       | otherwise = downHeap n i >> heapifyRoots (i - 1)
+
+
+    {-# INLINE exch #-}
+    exch ix1 ix2 | ix1 == ix2   = return ()
+                 | otherwise    = do
+      let i = ix1 + mn
+          j = ix2 + mn
+      v1 <- readArray a i
+      v2 <- readArray a j
+      writeArray a j v1
+      writeArray a i v2
+
+    {-# INLINE extract #-}
+    extract = extractRoot (getN - 1)
+      where
+        extractRoot k
+          | k <= 0    = return ()
+          | otherwise = do
+              exch k 0
+              downHeap k 0
+              extractRoot (k - 1)
--- hackerrank/cookie/Vec.hs
+++ hackerrank/cookie/Vec.hs
@@ -21,19 +21,32 @@ run' h = do
   let v = V.fromList cs
   print $ V.take 10 $ v
 
+cmp :: Ord a
+  => a -> a -> Ordering
+cmp a b =
+  case compare a b of
+    EQ -> EQ
+    LT -> GT
+    GT -> LT
+
 -- run1 :: IO (MV.MVector s a)
 run1 = do
-  let size = 6
+  let size = length xs
+      xs = [1,9,10,2,12,3,33,32,89,47,75,90,44]
+      p v = forM_ [0..size-1] $ \i -> print =<< (MV.unsafeRead v i)
   v <- MV.new size
-  forM_ (Prelude.zip [0..size-1] [1,9,10,2,12,3]) $ \(i, e) ->
+  forM_ (Prelude.zip [0..size-1] xs) $ \(i, e) ->
     MV.unsafeWrite v i e
-  VH.sort v
-  forM_ [0..size-1] $ \i -> print =<< (MV.unsafeRead v i)
-  putStrLn "==="
-  -- pop compare v 0 size-1
-  -- heapify compare v 0 size-1
 
-  forM_ [0..size-1] $ \i -> print =<< (MV.unsafeRead v i)
+  p v
+  -- VH.sortBy cmp v
+  VH.sortHeap cmp v 0 1 size
+  -- VH.heapify compare v 0 size
+  putStrLn "==="
+  -- VH.pop compare v 0 size
+  -- VH.heapify compare v 0 (size-1)
+
+  p v
   return v
 
 readWords :: Handle -> IO (Either String [Word])
