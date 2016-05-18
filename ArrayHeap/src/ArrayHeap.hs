390864e91bfd3f1e8cea4d668f93aa2686ff3600
Ayman Osman <aymano.osman@gmail.com>
2016-05-18 18:43:11 +0100

    wip

--- ArrayHeap/src/ArrayHeap.hs
+++ ArrayHeap/src/ArrayHeap.hs
@@ -6,7 +6,7 @@ import System.Environment (getArgs)
 import Control.Monad.ST (ST)
 import Data.Array (Array)
 import Data.Array.Unboxed (UArray)
-import Data.Array.IArray (elems, indices, (!))
+import Data.Array.IArray (IArray, indices, (!))
 import Data.Array.ST (STArray, Ix)
 import qualified Data.Array.ST as Array
 
@@ -15,11 +15,14 @@ main = do
   [which, x] <- getArgs
   case which of
     "slow" ->
-      let arr = mkArr_slow $ read x
-      in print $ foldA_slow (+) 0 arr
+      let arr = mkArr $ read x
+      in print $ foldASlow (+) 0 arr
     "fast" ->
       let arr = mkArr $ read x
       in print $ foldA (+) 0 arr
+
+    _ ->
+      putStrLn "Usage: [slow|fast] num"
 
 
 asd ::(Ix i)
@@ -27,28 +30,25 @@ asd ::(Ix i)
 asd = Array.runSTArray
 
 mkArr :: Int -> UArray Int Int
-mkArr x = Array.runSTUArray $ do
-  arr <- Array.newListArray (1,x) [1..x]
-  -- Array.writeArray arr 1 (42::Int)
-  return arr
+mkArr x = Array.runSTUArray $
+  Array.newListArray (1,x) [1..x]
 
-mkArr_slow :: Int -> UArray Int Int
-mkArr_slow x = Array.runSTUArray $ do
-  -- let xs = [1..x]
-  -- arr <- Array.newListArray (1,length xs) xs
-  arr <- Array.newListArray (1,x) [1..x]
-  -- Array.writeArray arr 1 (42::Int)
-  return arr
+-- mkArrSlow :: Int -> UArray Int Int
+-- mkArrSlow x = Array.runSTUArray $ do
+--   let xs = [1..x]
+--   Array.newListArray (1,length xs) xs
 
-foldA f s a =
-  go s (indices a)
+foldA :: (Ix i, IArray a e) => (t -> e -> t) -> t -> a i e -> t
+foldA f s' a =
+  go s' (indices a)
   where
     go !s (j:js) =
       go (f s (a!j)) js
     go s _ = s
 
-foldA_slow f s a =
-  go s (indices a)
+foldASlow :: (Ix i, IArray a e) => (t -> e -> t) -> t -> a i e -> t
+foldASlow f s' a =
+  go s' (indices a)
   where
     go s (j:js) =
       go (f s (a!j)) js
